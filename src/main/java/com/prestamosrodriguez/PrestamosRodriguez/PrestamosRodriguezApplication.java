2e337f856dab7bf389e44eea990776fbdbbece03
Cedamanos1 <jcedamanosg1@upao.edu.pe>
2025-12-06 22:31:59 -0500

    hola

---
+++ src/main/java/com/prestamosrodriguez/PrestamosRodriguez/PrestamosRodriguezApplication.java
@@ -0,0 +1,13 @@
+package com.prestamosrodriguez.PrestamosRodriguez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamosRodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamosRodriguezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prestamosrodriguez/PrestamosRodriguez/PrestamosRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestamosrodriguez.PrestamosRodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamosRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
