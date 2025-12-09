63df798c66a9120f29cb482f383e0d6c12722526
jcedamanosg1 <jcedamanosg1@upao.edu.pe>
2025-12-09 22:23:59 +0000

    ultimo error supuestamente

--- src/main/java/com/academiabaile/back/config/PaypalConfig.java
+++ src/main/java/com/academiabaile/back/config/PaypalConfig.java
@@ -1,15 +1,10 @@
 package com.academiabaile.back.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.paypal.base.rest.APIContext;
-import com.paypal.base.rest.OAuthTokenCredential;
-import com.paypal.base.rest.PayPalRESTException;
 
 @Configuration
 public class PaypalConfig {
@@ -24,21 +19,7 @@ public class PaypalConfig {
     private String mode;
 
     @Bean
-    public Map<String, String> paypalSdkConfig() {
-        Map<String, String> configMap = new HashMap<>();
-        configMap.put("mode", mode);
-        return configMap;
-    }
-
-    @Bean
-    public OAuthTokenCredential oAuthTokenCredential() {
-        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
-    }
-
-    @Bean
     public APIContext apiContext() throws PayPalRESTException {
-        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
-        context.setConfigurationMap(paypalSdkConfig());
-        return context;
+        return new APIContext(clientId, clientSecret, mode);
     }
 }
