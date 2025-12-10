99c454769dd8fda2dc681fc36fd36d69d0f82aa8
leefernandez1245 <leefernandez1245@gmail.com>
2025-12-10 03:44:07 +0000

    install

--- src/main/java/com/academiabaile/back/config/MercadoPagoApiConfig.java
+++ src/main/java/com/academiabaile/back/config/MercadoPagoApiConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MercadoPagoConfig {
+public class MercadoPagoApiConfig {
 
     @Value("${mercadopago.access.token}")
     private String accessToken;
